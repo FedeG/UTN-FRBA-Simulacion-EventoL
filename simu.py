e7284ee9d6b97e5452aceb15ead8cee4423e5df9
FedeG <federicogonzalez761@gmail.com>
2023-05-07 12:10:16 -0300

    Add dynamic queue

--- simu.py
+++ simu.py
@@ -3,6 +3,8 @@ import random
 from scipy.stats import lognorm
 
 
+HV = float('inf')
+
 def get_IA():
     r = random.uniform(0, 0.9999999)
     mu = 9.32  # Media
@@ -41,17 +43,17 @@ def get_TA_extra_por_instalar():
     return random.uniform(5, 10)
 
 
-def get_proxima_salida(TPS):
-    return TPS.index(min(TPS))
+def get_proxima_salida(tps):
+    return tps.index(min(tps))
 
 
-def get_puesto_ocioso_hace_mas_tiempo(TPS, ITO):
-    libres = [index for index, time in enumerate(TPS) if time == float('inf')]
+def get_puesto_ocioso_hace_mas_tiempo(tps, ito):
+    libres = [index for index, time in enumerate(tps) if time == HV]
 
     if len(libres) == 1:
         return libres[0]
 
-    itos_index = [(ITO[index], index) for index in libres]
+    itos_index = [(ito[index], index) for index in libres]
     itos_index_sorted = sorted(itos_index, key=lambda x: x[0])
     return itos_index_sorted[0][1]
 
@@ -83,19 +85,31 @@ def get_arrepentimiento(NS, N):
     return NS - N >= 3
 
 
-def run(N, TF, log=True):
-    HV = float('inf')
+def usa_intermitente(NS, N):
+    return NS - N >= 2
+
+
+def intermitentes_ocupados(tpsi):
+    return len([tps for tps in tpsi if tps != HV])
+
+
+def run(N, M, TF, log=True):
     TPLL = get_primera_llegada()
     TPS = [HV] * N
+    TPSI = [HV] * M
     NS = 0
     NT = 0
     AR = 0
     STS = 0
     STLL = 0
     STA = 0
+    STAI = [0] * M
     ITO = [0] * N
+    ITOI = [0] * M
     STO = [0] * N
+    STOI = [0] * M
     PTO = [HV] * N
+    PTOI = [HV] * M
     T = 0
 
     while T <= TF or NS > 0:
@@ -103,13 +117,14 @@ def run(N, TF, log=True):
             print(f'T: {T} TF: {TF} NS: {NS} NT: {NT} TPLL: {TPLL} TPS: {TPS}')
 
         i = get_proxima_salida(TPS)
-        if TPLL <= TPS[i]:
+        j = get_proxima_salida(TPSI) if M > 0 else -1
+        if (M > 0 and TPLL <= TPS[i] and TPLL <= TPSI[j]) or (M == 0 and TPLL <= TPS[i]):
             # llegada
             T = TPLL
             IA = get_IA()
             TPLL += IA
 
-            arrepentimiento = get_arrepentimiento(NS, N)
+            arrepentimiento = get_arrepentimiento(NS, N + intermitentes_ocupados(TPSI))
             if arrepentimiento:
                 AR += 1
                 continue
@@ -118,25 +133,48 @@ def run(N, TF, log=True):
             NT += 1
             STLL += T
 
-            if NS <= N:
+            if NS <= N + intermitentes_ocupados(TPSI):
                 k = get_puesto_ocioso_hace_mas_tiempo(TPS, ITO)
                 TA = get_TA()
-                TPS[k] = T + TA
+                TPS[k] = T + TA+ intermitentes_ocupados(TPSI)
                 STO[k] = STO[k] + (T - ITO[k])
                 STA += TA
+            elif M > 0 and usa_intermitente(NS, N + intermitentes_ocupados(TPSI)) and NS <= N + M + 2:
+                if len([tps for tps in TPSI if tps == HV]) > 0:
+                    k = get_puesto_ocioso_hace_mas_tiempo(TPSI, ITOI)
+                    TA = get_TA()
+                    TPSI[k] = T + TA
+                    STOI[k] = STOI[k] + (T - ITOI[k])
+                    STA += TA
+                    STAI[k] += TA
         else:
             # salida
-            T = TPS[i]
-            STS += T
-            NS -= 1
+            if M > 0 and TPSI[j] <= TPS[i]:
+                # salida intermitente
+                T = TPSI[j]
+                STS += T
+                NS -= 1
 
-            if NS >= N:
-                TA = get_TA()
-                TPS[i] = T + TA
-                STA += TA
-            else:
-                TPS[i] = HV
-                ITO[i] = T
+                if usa_intermitente(NS, N + intermitentes_ocupados(TPSI) - 1):
+                    TA = get_TA()
+                    TPSI[j] = T + TA
+                    STA += TA
+                else:
+                    TPSI[j] = HV
+                    ITOI[j] = T
+            else:    
+                # salida fijos
+                T = TPS[i]
+                STS += T
+                NS -= 1
+
+                if NS >= N + intermitentes_ocupados(TPSI):
+                    TA = get_TA()
+                    TPS[i] = T + TA
+                    STA += TA
+                else:
+                    TPS[i] = HV
+                    ITO[i] = T
         if T > TF:
             TPLL = HV
 
@@ -154,13 +192,15 @@ def run(N, TF, log=True):
             print(f'Puesto {l}: {PTO[l]}%')
         print(f'Porcentaje de arrepentimiento es de {PA}%')
         print(f'Promedio de espera en cola: {PEC} segundos')
+        if M > 0:
+            print(f'Tiempos de atención en intermitentes: {STAI} segundos')
 
     return N, TF, PTO, PA, PEC
 
 
-def run_n_times(N, TF, n):
+def run_n_times(N, M, TF, n):
     # results = [(N, TF, PTO, PA, PEC)]
-    results = [run(N, TF, log=False) for _ in range(n)]
+    results = [run(N, M, TF, log=True) for _ in range(n)]
 
     print(f'La variable de control es: {N}')
     print(f'Porcentaje de tiempo ocioso por puesto')
